46679d1dd8182036bd778c5e1c4dd161fd8fe0a9
MarkMT <nzl216@gmail.com>
2024-12-18 16:59:16 +1300

    Update asset_url_processor.rb

--- lib/sprockets/rails/asset_url_processor.rb
+++ lib/sprockets/rails/asset_url_processor.rb
@@ -9,7 +9,7 @@ module Sprockets
         context = input[:environment].context_class.new(input)
         data    = input[:data].gsub(REGEX) do |_match|
           #puts "***** _match #{_match.inspect}"
-          #path = Regexp.last_match[:path]
+          path = Regexp.last_match[:path]
           #puts "***** path #{path.inspect}"
           #puts "***** url(#{context.asset_path(path)})"
           "url(#{context.asset_path(path)})"
